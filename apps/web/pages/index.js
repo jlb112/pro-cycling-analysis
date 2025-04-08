6541ed9c46162d0725d459f9de0b906cb0200fd0
jlb112 <j.breakey@hotmail.com>
2025-04-08 10:03:57 +0100

    Setup changes

---
+++ apps/web/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🚴‍♂️ Pro Cycling Analysis 2</h1>
+      <p>Welcome to your Next.js site!</p>
+    </div>
+  );
+}
+  
